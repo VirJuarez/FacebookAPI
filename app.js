b880fb1a1a93343098dd2df3d3a8dd35f1bae87e
Virignia <virginia.juarez1996@gmail.com>
2024-03-03 20:05:24 -0300

    correción archivo app

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fbRouter = require('./routes/fbRouter');
 
-//const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use((_, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
@@ -20,12 +19,6 @@ app.use((_, res, next) => {
   next();
 });
 app.use('/facebook', fbRouter);
-
-app.use((err, _req, res, _next) => {
-  console.error(err);
-  res.status(500).send(err); // Envía el error completo como respuesta al cliente
-});
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
